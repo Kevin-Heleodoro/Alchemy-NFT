d1e6990ab5c5fa3a5e1a276f3c6470beafc5bee3
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-04-26 18:23:17 -0500

    compiled contract. Had to set the solidity version to 0.7.13 in MyNFT.sol for it to work

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
+	solidity: '0.7.3',
+	// {
+	// compilers: [
+	// 	{
+	// 		version: '0.7.3',
+	// 	},
+	// 	{
+	// 		version: '0.8.13',
+	// 	},
+	// ],
+	// },
+	defaultNetwork: 'ropsten',
+	networks: {
+		hardhat: {},
+		ropsten: {
+			url: API_URL,
+			accounts: [`0x${PRIVATE_KEY}`],
+		},
+	},
 };
